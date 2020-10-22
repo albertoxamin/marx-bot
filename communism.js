ab0879bb3237afb505d83fde625bd0cc82c717de
Matteo Zanella <matteo.zanella98@gmail.com>
2020-10-22 18:09:01 +0200

    Spanish support

--- communism.js
+++ communism.js
@@ -3,6 +3,10 @@ const mio = /(?<!\w)(mio|tuo|suo|vostro)(?=\s|$)/gi;
 const mia = /(?<!\w)(mia|tua|sua|vostra)(?=\s|$)/gi;
 const miei = /(?<!\w)(miei|tuoi|suoi|vostri)(?=\s|$)/gi;
 const mie = /(?<!\w)(mie|tue|sue|vostre)(?=\s|$)/gi;
+const nuestro = /(?<!\w)(mi|tu|vuestro|su)(?=\s|$)/gi;
+const nuestros = /(?<!\w)(mis|tus|vuestros|sus)(?=\s|$)/gi;
+const nuestra = /(?<!\w)(vuestra)(?=\s|$)/gi;
+const nuestras = /(?<!\w)(vuestras)(?=\s|$)/gi;
 
 module.exports = {
 	marxify: (msg) => {
@@ -13,6 +17,11 @@ module.exports = {
 		nms = nms.replace(mia, 'NOSTRA');
 		nms = nms.replace(miei, 'NOSTRI');
 		nms = nms.replace(mie, 'NOSTRE');
+		//Spanish part
+		nms = nms.replace(nuestro, 'NUESTRO');
+		nms = nms.replace(nuestros, 'NUESTROS');
+		nms = nms.replace(nuestra, 'NUESTRA');
+		nms = nms.replace(nuestras, 'NUESTRAS');
 
 		return nms;
 	}
