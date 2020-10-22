47880a323e9916a076153a1e2260e50c048d7125
Matteo Zanella <matteo.zanella98@gmail.com>
2020-10-22 18:15:14 +0200

    German support

--- communism.js
+++ communism.js
@@ -3,6 +3,8 @@ const mio = /(?<!\w)(mio|tuo|suo|vostro)(?=\s|$)/gi;
 const mia = /(?<!\w)(mia|tua|sua|vostra)(?=\s|$)/gi;
 const miei = /(?<!\w)(miei|tuoi|suoi|vostri)(?=\s|$)/gi;
 const mie = /(?<!\w)(mie|tue|sue|vostre)(?=\s|$)/gi;
+const unser = /(?<!\w)(mein|dein|sein|ihr|euer)(?=\s|$)/gi;
+const unsere = /(?<!\w)(meine|deine|seine|ihre|eure|euere)(?=\s|$)/gi;
 
 module.exports = {
 	marxify: (msg) => {
@@ -13,6 +15,9 @@ module.exports = {
 		nms = nms.replace(mia, 'NOSTRA');
 		nms = nms.replace(miei, 'NOSTRI');
 		nms = nms.replace(mie, 'NOSTRE');
+		// German support
+		nms = nms.replace(unser, 'UNSER');
+		nms = nms.replace(unsere, 'UNSERE');
 
 		return nms;
 	}
