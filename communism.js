24c253abe8517e6f177c83a285d4a09527c5ac63
Matteo Zanella <matteo.zanella98@gmail.com>
2020-10-22 17:56:56 +0200

    French support

--- communism.js
+++ communism.js
@@ -3,17 +3,21 @@ const mio = /(?<!\w)(mio|tuo|suo|vostro)(?=\s|$)/gi;
 const mia = /(?<!\w)(mia|tua|sua|vostra)(?=\s|$)/gi;
 const miei = /(?<!\w)(miei|tuoi|suoi|vostri)(?=\s|$)/gi;
 const mie = /(?<!\w)(mie|tue|sue|vostre)(?=\s|$)/gi;
+const notre = /(?<!\w)(mon|ma|ton|ta|son|sa|votre|leur)(?=\s|$)/gi;
+const nos = /(?<!\w)(mes|tes|ses|vos|leurs)(?=\s|$)/gi;
 
 module.exports = {
 	marxify: (msg) => {
+		// English part
 		let nms = msg.replace(my, 'OUR');
-
-		//italian part
+		// Italian part
 		nms = nms.replace(mio, 'NOSTRO');
 		nms = nms.replace(mia, 'NOSTRA');
 		nms = nms.replace(miei, 'NOSTRI');
 		nms = nms.replace(mie, 'NOSTRE');
-
+		// French part
+		nms = nms.replace(notre, 'NOTRE');
+		nms = nms.replace(nos, 'NOS');
 		return nms;
 	}
 }
